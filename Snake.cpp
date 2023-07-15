696af5991b94321bacd0fb14c6ab3318006751b5
jacob-a2k <jakub.a2k@gmail.com>
2023-07-15 07:54:52 +0200

    one part of snake follows the other

--- Snake.cpp
+++ Snake.cpp
@@ -1,77 +1,119 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <conio.h>
+#include <Windows.h>
 
-class Node {
-	char partOfSnake;
-	Node* next;
+class Point {
 public:
-	Node(char part = 'o', Node* next = nullptr) : partOfSnake(part), next(next) {}
-	Node* getNext() const {
-		return next;
-	}
-	void fillNext(Node* newNext) { this->next = newNext; }
+	int x;
+	int y;
+	Point(int x = NULL, int y = NULL) : x(x), y(y){}
 };
 
-class Snake {
-	Node* head;
-	Node* tail;
-public:
-	Snake(Node* head = nullptr, Node* tail = nullptr) : head(head), tail(tail) {}
-	void fillhead(Node* newHead) { this->head = newHead; }
-	void filltail(Node* newTail) { this->tail = newTail; }
-	Node* getHead() const {
-		return head;
-	}
-	Node* getTail() const {
-		return tail;
-	}
-};
-
-class Map {
-	char map[30][120];
-public:
-	Map();
-	void refillMap(char newMap[][120]);
-	void printMap(char actualMap[][120]);
-};
-Map::Map() {
-	map[0][0] = '+';
-	for (int i = 1; i < 119; ++i) {
-		map[0][i] = '-';
-	}
-	map[0][119] = '+';
-	for (int j = 1; j < 29; ++j) {
-		map[j][0] = '|';
-		for (int i = 1; i < 119; ++i) {
-			map[j][i] = ' ';
-		}
-		map[j][119] = '|';
-	}
-	map[29][0] = '+';
-	for (int i = 1; i < 119; ++i) {
-		map[29][i] = '-';
-	}
-	map[29][119] = '+';
-}
-void Map::refillMap(char newMap[][120]) {
-	for (int i = 0; i < 30; i++) {
-		for (int j = 0; j < 120; j++) {
-			map[i][j] = newMap[i][j];
-		}
-	}
-}
-void Map::printMap(char actualMap[][120]) {
-	for (int i = 0; i < 30; ++i) {
-		for (int j = 0; j < 120; ++j) {
-			std::cout << actualMap[i][j];
-		}
-		std::cout << '\n';
-	}
-}
+void fillMap(char gameMap[][120]);
+void printMap(char gameMap[][120]);
 
 int main() {
 
-	Snake snake_ptr;
+	Point coordinate[3600];
+	char gameMap[30][120];
+	fillMap(gameMap);
+	int snakeSize = 10;
+	char partOfSnake = 'o';
 
+	int nextXpos = 10;
+	int nextYpos = 10;
+	int tmpPosX = nextXpos;
+	int tmpPosY = nextYpos;
 
+	coordinate[0].x = nextXpos;
+	coordinate[0].y = nextYpos;
+
+	char direction = 'd';
+	bool error = false;
+
+	int XposBeforeLoop;
+	int YposBeforeLoop;
+	int tmpX;
+	int tmpY;
+	
+	while (!error) {
+		if (_kbhit()) {
+			direction = _getch();
+		}
+		switch (direction) {
+		case 'd':
+			nextXpos++;
+			break;
+		case 's':
+			nextYpos++;
+			break;
+		case 'w':
+			nextYpos--;
+			break;
+		case 'a':
+			nextXpos--;
+			break;
+		default:
+			std::cout << "Something goes wrong!!!" << std::endl;
+			error = true;
+		}
+		XposBeforeLoop = nextXpos;
+		YposBeforeLoop = nextYpos;
+		
+		for (int i = 0; i < snakeSize; i++) {
+			tmpX = coordinate[i].x;
+			tmpY = coordinate[i].y;
+			gameMap[coordinate[i].y][coordinate[i].x] = ' ';
+			coordinate[i].x = nextXpos;
+			coordinate[i].y = nextYpos;
+			gameMap[coordinate[i].y][coordinate[i].x] = 'o';
+			nextXpos = tmpX;
+			nextYpos = tmpY;
+		}
+		nextXpos = XposBeforeLoop;
+		nextYpos = YposBeforeLoop;
+
+		
+
+		//for (int i = 0; i < snakeSize; i++) {
+		//	tmpPosX = coordinate[i].x;
+		//	tmpPosY = coordinate[i].y;
+		//	gameMap[coordinate[i].y][coordinate[i].x] = ' ';
+		//	coordinate[i].x = nextXpos;
+		//	coordinate[i].y = nextYpos;
+		//	gameMap[coordinate[i].y][coordinate[i].x] = partOfSnake;
+		//}
+		printMap(gameMap);
+		Sleep(50);
+		system("cls");
+	}
+
+	//printMap(gameMap);
+}
+void fillMap(char gameMap[][120]) {
+	gameMap[0][0] = '+';
+	for (int i = 1; i < 119; ++i) {
+		gameMap[0][i] = '-';
+	}
+	gameMap[0][119] = '+';
+	for (int j = 1; j < 29; ++j) {
+		gameMap[j][0] = '|';
+		for (int i = 1; i < 119; ++i) {
+			gameMap[j][i] = ' ';
+		}
+		gameMap[j][119] = '|';
+	}
+	gameMap[29][0] = '+';
+	for (int i = 1; i < 119; ++i) {
+		gameMap[29][i] = '-';
+	}
+	gameMap[29][119] = '+';
+}
+void printMap(char gameMap[][120]) {
+	for (int i = 0; i < 30; i++) {
+		for (int j = 0; j < 120; j++) {
+			std::cout << gameMap[i][j];
+		}
+		std::cout << '\n';
+	}
 }
